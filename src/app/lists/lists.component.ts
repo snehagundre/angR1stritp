64daa2b3ae176c062c44e165e4a2c086d6ffe9af
snehagundre <118663855+snehagundre@users.noreply.github.com>
2025-11-03 15:48:53 +0530

    modified

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -14,61 +14,61 @@ export class ListsComponent implements OnInit {
 
 // 1️⃣ Products
  productsText = 'Top 10 Popular Products You Can Buy Online';
- products = [
+ products :Array<string>=  [
   'Laptop', 'Watch', 'Pen', 'Book', 'Mobile', 'Bag', 'Shoes', 'Tablet', 'Camera', 'Charger'
 ];
 
 // 2️⃣ Fruits
  fruitsText = 'List of Delicious and Healthy Fruits';
- fruits = [
+ fruits:Array<string> = [
   'Apple', 'Banana', 'Mango', 'Orange', 'Pineapple', 'Grapes', 'Strawberry', 'Papaya', 'Watermelon', 'Kiwi'
 ];
 
 // 3️⃣ Colors
  colorsText = '10 Common Colors Used in Design';
- colors = [
+ colors:Array<string> = [
   'Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Black', 'White', 'Gray'
 ];
 
 // 4️⃣ Cities
  citiesText = 'Top 10 Cities in India You Should Know About';
- cities = [
+ cities:Array<string>  = [
   'Mumbai', 'Delhi', 'Pune', 'Chennai', 'Hyderabad', 'Bangalore', 'Kolkata', 'Surat', 'Jaipur', 'Indore'
 ];
 
 // 5️⃣ Subjects
  subjectsText= '10 Important School Subjects for Students';
- subjects= [
+ subjects :Array<string> = [
   'Math', 'Science', 'English', 'History', 'Geography', 'Computer', 'Economics', 'Physics', 'Chemistry', 'Biology'
 ];
 
 // 6️⃣ Vehicles
  vehiclesText = 'Different Types of Vehicles We Use Every Day';
- vehicles = [
+ vehicles:Array<string>  = [
   'Car', 'Bike', 'Scooter', 'Bus', 'Truck', 'Train', 'Bicycle', 'Boat', 'Helicopter', 'Airplane'
 ];
 
 // 7️⃣ Animals
  animalsText = '10 Popular Animals Found Around the World';
- animals= [
+ animals:Array<string> = [
   'Dog', 'Cat', 'Elephant', 'Tiger', 'Lion', 'Zebra', 'Horse', 'Deer', 'Monkey', 'Rabbit'
 ];
 
 // 8️⃣ Sports
  sportsText = 'Most Popular Sports Played Globally';
-sports= [
+sports:Array<string> = [
   'Cricket', 'Football', 'Tennis', 'Badminton', 'Hockey', 'Volleyball', 'Basketball', 'Baseball', 'Golf', 'Table Tennis'
 ];
 
 // 9️⃣ Countries
  countriesText = '10 Famous Countries Across the World';
- countries = [
+ countries:Array<string>  = [
   'India', 'USA', 'Canada', 'Japan', 'Germany', 'France', 'Brazil', 'Australia', 'China', 'Italy'
 ];
 
 // 🔟 Languages
  languagesText = 'Major Languages Spoken Across India';
- languages= [
+ languages:Array<string> = [
   'English', 'Hindi', 'Marathi', 'Gujarati', 'Tamil', 'Telugu', 'Bengali', 'Kannada', 'Punjabi', 'Malayalam'
 ];
 
